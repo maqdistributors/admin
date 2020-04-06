bffb2fa6748486c8b3dc2a84f828c22396518a1b
bista <jaykishan.sharma@bistasolutions.com>
2020-04-06 19:53:06 +0530

    [ADD] Added the improvements of maq_account module.

--- maq_account/__manifest__.py
+++ maq_account/__manifest__.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'MAQ Accounting',
+    "version": "1.0",
     'summary': 'Customized and improve a Accounting features',
     'category': 'Accounting',
     'description': """Following accounting features will be added
         Req Id. 1.13 - Show records in ascending in Account Bank reconcilation Report
     """,
-    'depends': ['account_reports'],
+    'depends': ['account'],
     'data': [],
     'qweb': [],
     'auto_install': True,
