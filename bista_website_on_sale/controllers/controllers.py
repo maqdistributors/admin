c62cb1e9b1c5c7e3ef9ac0301792cb229d84dea2
rahul <rahul.malve@bistasolutions.com>
2020-06-17 21:47:17 +0530

    [RSM Add] quick fixed for sale page for min_quantity > 1

--- bista_website_on_sale/controllers/controllers.py
+++ bista_website_on_sale/controllers/controllers.py
@@ -107,6 +107,8 @@ class WebsiteSale(WebsiteSale):
                     sale_product = pricelist_item.product_tmpl_id.id
                     if pricelist_item.min_quantity in [0,1] and pricelist_item.date_start == False and pricelist_item.date_end == False:
                         continue
+                    elif pricelist_item.min_quantity > 1 and pricelist_item.date_start == False and pricelist_item.date_end == False:
+                        continue
                     elif pricelist_item.min_quantity in [0,1] and pricelist_item.date_end:
                         if (date_start_date == datetime.now().date() or date_start_date < datetime.now().date()) and (
                                     date_end_date == datetime.now().date() or date_end_date > datetime.now().date()):
