f0faa97ce365b277b0bc5fa3cb203085499c328f
rahul <rahul.malve@bistasolutions.com>
2020-06-19 11:35:20 +0530

    [RSM Add] Sort by New product feature development for website shop

---
+++ bista_website_sortby_new/models/product_template.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+import logging
+
+from odoo import api, fields, models, tools, _
+from datetime import datetime
+from odoo.addons import decimal_precision as dp
+# from odoo.exceptions import UserError, ValidationError
+# from datetime import datetime
+
+_logger = logging.getLogger(__name__)
+
+class ProductTemplate(models.Model):
+    _inherit='product.template'
+
+    publish_date = fields.Datetime('Publish Date')
+
+    @api.multi
+    def write(self, vals):
+
+        if vals.get('website_published') == True:
+            vals['publish_date'] = datetime.now()
+        elif vals.get('website_published') == False:
+            vals['publish_date'] = self.create_date
+
+        result = super(ProductTemplate, self).write(vals)
+
+        return result
+
+
