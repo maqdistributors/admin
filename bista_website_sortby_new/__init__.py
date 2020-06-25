c754981bc7a1735afe3d11889cccfc038f576867
rahul <rahul.malve@bistasolutions.com>
2020-06-25 13:19:59 +0530

    [RSM Add] Releasing sort by new product development

---
+++ bista_website_sortby_new/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from . import models
+
+from odoo import api, SUPERUSER_ID
+
+_logger = logging.getLogger(__name__)
+
+def _update_publish_date(cr, registry):
+
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    products = env['product.template'].search([])
+
+    for product in products:
+        product_vals = {}
+        product_name = str(product.name) or ''
+        product_vals.update({'publish_date':product.create_date})
+
+        if product_vals:
+            try:
+                product.write(product_vals)
+            except Exception as e:
+                _logger.error(
+                    'product name - ' + product_name + ' : %s', e)
+                pass
