1c49ee899230414cca4b236af588f95900130bd8
bista <jaykishan.sharma@bistasolutions.com>
2020-06-29 15:48:19 +0530

    [ADD] Added the confirm alert dialogbox functionality in pos.

---
+++ maq_point_of_sale/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'MAQ Point of Sale',
+    'version' : '11.0.1.0.0',
+    'summary': 'Point of sale',
+    'description': """
+    Point of Sale Custom code
+    """,
+    'category': 'POS',
+    'website': 'https://www.bistasolutions.com/',
+    'depends' : ['point_of_sale'],
+    'data': [
+        'views/assets.xml',
+        'views/pos_order_view.xml'
+    ],
+    'demo': [
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
