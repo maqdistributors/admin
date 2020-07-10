6114a81670957f71fab8b3d0933365ce83ffe251
rahul <rahul.malve@bistasolutions.com>
2020-07-10 15:03:30 +0530

    [RSM Add] Shipping cart order line price update development

---
+++ bista_shipping_cart_update/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Bista Website Shipping Cart Update",
+    "version": "11.0.1.0.0",
+    "author": "Bista Solutions Pvt. Ltd.",
+    "maintainer": "Bista Solutions Pvt. Ltd.",
+    "website": "https://www.bistasolutions.com",
+    "category": "Web",
+    "license": "AGPL-3",
+    'summary': """This module contains following features
+                1. Website shipping cart product details update""",
+    "depends": [
+        "website_sale"
+    ],
+    # "data": [
+    #     "views/website_sale.xml",
+    #     "views/product_template_views.xml",
+    # ],
+    #    "qweb": [
+    #        "static/src/xml/website_sale.xml",
+    #    ],
+    # "images": ["static/description/groupexpand.png"],
+    "installable": True,
+}
+
---
+++ bista_shipping_cart_update/controllers/controllers.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from odoo import http, tools, _
+from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+
+_logger = logging.getLogger(__name__)
+
+class WebsiteSale(WebsiteSale):
+
+    def recalculate_prices(self, order):
+        for line in order.mapped('order_line'):
+            dict = line._convert_to_write(line.read()[0])
+            if 'product_tmpl_id' in line._fields:
+                dict['product_tmpl_id'] = line.product_tmpl_id
+            line2 = request.env['sale.order.line'].new(dict)
+            # we make this to isolate changed values:
+            line2.product_uom_change()
+            line2._onchange_discount()
+            line.write({
+                'price_unit': line2.price_unit,
+                'discount': line2.discount,
+            })
+        return True
+
+    @http.route(['/shop/cart'], type='http', auth="public", website=True)
+    def cart(self, access_token=None, revive='', **post):
+        """
+        Main cart management + abandoned cart revival
+        access_token: Abandoned cart SO access token
+        revive: Revival method when abandoned cart. Can be 'merge' or 'squash'
+        """
+        order = request.website.sale_get_order()
+        if order and order.state != 'draft':
+            request.session['sale_order_id'] = None
+            order = request.website.sale_get_order()
+        values = {}
+        if access_token:
+            abandoned_order = request.env['sale.order'].sudo().search([('access_token', '=', access_token)], limit=1)
+            if not abandoned_order:  # wrong token (or SO has been deleted)
+                return request.render('website.404')
+            if abandoned_order.state != 'draft':  # abandoned cart already finished
+                values.update({'abandoned_proceed': True})
+            elif revive == 'squash' or (revive == 'merge' and not request.session[
+                'sale_order_id']):  # restore old cart or merge with unexistant
+                request.session['sale_order_id'] = abandoned_order.id
+                return request.redirect('/shop/cart')
+            elif revive == 'merge':
+                abandoned_order.order_line.write({'order_id': request.session['sale_order_id']})
+                abandoned_order.action_cancel()
+            elif abandoned_order.id != request.session[
+                'sale_order_id']:  # abandoned cart found, user have to choose what to do
+                values.update({'access_token': abandoned_order.access_token})
+
+        # order_lines = order.order_line
+        #
+        # for line in order_lines:
+        #     if line.exists():
+        #         print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>line.product_id.id",line.product_id.id)
+        #         order._cart_update(product_id=line.product_id.id, line_id=line.id, add_qty=0)
+        self.recalculate_prices(order)
+
+        if order:
+            from_currency = order.company_id.currency_id
+            to_currency = order.pricelist_id.currency_id
+            compute_currency = lambda price: from_currency.compute(price, to_currency)
+        else:
+            compute_currency = lambda price: price
+
+        values.update({
+            'website_sale_order': order,
+            'compute_currency': compute_currency,
+            'suggested_products': [],
+        })
+        if order:
+            _order = order
+            if not request.env.context.get('pricelist'):
+                _order = order.with_context(pricelist=order.pricelist_id.id)
+            values['suggested_products'] = _order._cart_accessories()
+
+        if post.get('type') == 'popover':
+            # force no-cache so IE11 doesn't cache this XHR
+            return request.render("website_sale.cart_popover", values, headers={'Cache-Control': 'no-cache'})
+
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>shipping cart", values)
+
+        return request.render("website_sale.cart", values)
