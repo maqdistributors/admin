f9606b9c6f8882f04c32b6b36ff0fbc53ed079bf
bista <jaykishan.sharma@bistasolutions.com>
2020-07-24 15:29:21 +0530

    [IMP] Improve the stock warning pop-up scenario.

--- pos_product_available_negative/static/src/js/pos.js
+++ pos_product_available_negative/static/src/js/pos.js
@@ -39,6 +39,68 @@ odoo.define("pos_product_available_negative.pos", function (require) {
                 : false;
             return json;
         },
+        add_product: function (product, options) {
+            if (this._printed) {
+                this.destroy();
+                return this.pos.get_order().add_product(product, options);
+            }
+            this.assert_editable();
+            options = options || {};
+            var attr = JSON.parse(JSON.stringify(product));
+            attr.pos = this.pos;
+            attr.order = this;
+            var line = new models.Orderline({}, {pos: this.pos, order: this, product: product});
+
+            if (options.quantity !== undefined) {
+                line.set_quantity(options.quantity);
+            }
+
+            if (options.price !== undefined) {
+                line.set_unit_price(options.price);
+            }
+
+            //To substract from the unit price the included taxes mapped by the fiscal position
+            this.fix_tax_included_price(line);
+
+            if (options.discount !== undefined) {
+                line.set_discount(options.discount);
+            }
+
+            if (options.extras !== undefined) {
+                for (var prop in options.extras) {
+                    line[prop] = options.extras[prop];
+                }
+            }
+
+            var to_merge_orderline;
+            for (var i = 0; i < this.orderlines.length; i++) {
+                if (this.orderlines.at(i).can_be_merged_with(line) && options.merge !== false) {
+                    to_merge_orderline = this.orderlines.at(i);
+                }
+            }
+            if (to_merge_orderline) {
+                if (to_merge_orderline.product.type === "product") {
+                    if (!to_merge_orderline.notify_qty) {
+                        if (to_merge_orderline.quantity + 1 > line.product.qty_available) {
+                            this.pos.gui.show_popup("alertMsg", {
+                                title: _t("STOCK CHECK WARNING"),
+                                body: _t('This product quantity may be unavailable'),
+                                msg: _t('Please verify the product quantity available and notify a supervisor of any discrepancies.'),
+                            });
+                            to_merge_orderline.notify_qty = true;
+                        }
+                    }
+                }
+                to_merge_orderline.merge(line);
+            } else {
+                this.orderlines.add(line);
+            }
+            this.select_orderline(this.get_last_orderline());
+
+            if (line.has_product_lot) {
+                this.display_lot_popup();
+            }
+        },
     });
     screens.OrderWidget.include({
         set_value: function (val) {
@@ -48,15 +110,17 @@ odoo.define("pos_product_available_negative.pos", function (require) {
                 var mode = this.numpad_state.get('mode');
                 if (mode === 'quantity') {
                     if (val != "" && val != "remove") {
-                        if (!order.get_selected_orderline().notify_qty) {
-                            var qty_available = order.get_selected_orderline().product.qty_available;
-                            if (val > qty_available) {
-                                this.gui.show_popup("alertMsg", {
-                                    title: _t("STOCK CHECK WARNING"),
-                                    body: _t('This product quantity may be unavailable'),
-                                    msg: _t('Please verify the product quantity available and notify a supervisor of any discrepancies.'),
-                                });
-                                order.get_selected_orderline().notify_qty = true;
+                        if (order.get_selected_orderline().product.type === "product") {
+                            if (!order.get_selected_orderline().notify_qty) {
+                                var qty_available = order.get_selected_orderline().product.qty_available;
+                                if (val > qty_available) {
+                                    this.gui.show_popup("alertMsg", {
+                                        title: _t("STOCK CHECK WARNING"),
+                                        body: _t('This product quantity may be unavailable'),
+                                        msg: _t('Please verify the product quantity available and notify a supervisor of any discrepancies.'),
+                                    });
+                                    order.get_selected_orderline().notify_qty = true;
+                                }
                             }
                         }
                     }
@@ -101,6 +165,21 @@ odoo.define("pos_product_available_negative.pos", function (require) {
                         body: _t('This product quantity may be unavailable'),
                         msg: _t('Please verify the product quantity available and notify a supervisor of any discrepancies.'),
                     });
+                } else if (product.type === "product") {
+                    if (order.get_selected_orderline() !== undefined) {
+                        if (order.get_selected_orderline().product.id === product.id) {
+                            if (!order.get_selected_orderline().notify_qty) {
+                                if (order.get_selected_orderline().quantity + 1 > order.get_selected_orderline().product.qty_available) {
+                                    self.gui.show_popup("alertMsg", {
+                                        title: _t("STOCK CHECK WARNING"),
+                                        body: _t('This product quantity may be unavailable'),
+                                        msg: _t('Please verify the product quantity available and notify a supervisor of any discrepancies.'),
+                                    });
+                                    order.get_selected_orderline().notify_qty = true;
+                                }
+                            }
+                        }
+                    }
                 }
                 _.bind(click_product_handler_super, this)();
             };
